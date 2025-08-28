a9df49a91097466c63bcf9e43a533ea916652b22
raj18110-max <rajkadam18110@gmail.com>
2025-08-28 18:33:32 +0530

    Add files via upload

---
+++ Raj2.py
@@ -0,0 +1,12 @@
+'''
+n1=int(input("Enter the first number"))
+if n1%2==0:
+    print(n,"Even")
+else:
+    print(n,"Odd")
+
+total=0
+for i in range(1,51):
+    total=total+i
+print("The sum of number from 1 to 51 is :",total)
+'''
